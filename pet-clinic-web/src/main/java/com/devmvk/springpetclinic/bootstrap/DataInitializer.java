eb39d475325b46bbeeda1a347d04c51dd543e43d
devmvk <developer.varunkumar@gmail.com>
2022-01-14 16:57:28 +0530

    adds bootstap data

--- pet-clinic-web/src/main/java/com/devmvk/springpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/devmvk/springpetclinic/bootstrap/DataInitializer.java
@@ -1,5 +1,7 @@
 package com.devmvk.springpetclinic.bootstrap;
 
+import com.devmvk.springpetclinic.models.Owner;
+import com.devmvk.springpetclinic.models.Vet;
 import com.devmvk.springpetclinic.services.OwnerService;
 import com.devmvk.springpetclinic.services.VetService;
 import com.devmvk.springpetclinic.services.mapservice.OwnerMapService;
@@ -19,6 +21,13 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Owner owner1 = new Owner("varun", "madupu");
+        ownerService.save(owner1);
+
+        Vet vet1 = new Vet("david", "ram");
+        vetService.save(vet1);
+
 
     }
 }
